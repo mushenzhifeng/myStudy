bee88feadede3d048b6a3340d796dbd4916d7417
Greg Kou <mushenzhifeng@163.com>
2018-03-01 15:46:31 +0800

    read_configration_file.py
    read configration file

---
+++ read_configration_file.py
@@ -0,0 +1,16 @@
+from oslo_config import cfg
+opt_group = cfg.OptGroup(name='simple',
+                         title='A Simple Example')
+simple_opts = [
+    cfg.BoolOpt('enable',
+                default=False,
+                help=('True enables, False disables'))
+]
+
+CONF = cfg.CONF
+CONF.register_group(opt_group)
+CONF.register_opts(simple_opts, opt_group)
+
+if __name__ == "__main__":
+    CONF(default_config_files=['app.conf'])
+    print(CONF.simple.enable)
